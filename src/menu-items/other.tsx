a8b0fba3bebfd3d7b25fe7254e8177eafad13a29
besdima <dev.bespalov@gmail.com>
2023-02-20 18:09:09 +0800

    Initial commit

---
+++ src/menu-items/other.tsx
@@ -0,0 +1,42 @@
+// third-party
+import { FormattedMessage } from 'react-intl';
+
+// assets
+import { ChromeOutlined, QuestionOutlined, DeploymentUnitOutlined, CalendarOutlined } from '@ant-design/icons';
+
+// type
+import { NavItemType } from 'types/menu';
+
+// icons
+const icons = {
+  ChromeOutlined,
+  QuestionOutlined,
+  DeploymentUnitOutlined,
+  CalendarOutlined
+};
+
+// ==============================|| MENU ITEMS - SUPPORT ||============================== //
+
+const other: NavItemType = {
+  id: 'other',
+  title: <FormattedMessage id="others" />,
+  type: 'group',
+  children: [
+    {
+      id: 'sample-page',
+      title: <FormattedMessage id="sample-page" />,
+      type: 'item',
+      url: '/sample-page',
+      icon: icons.ChromeOutlined
+    },
+    {
+      id: 'calendar',
+      title: <FormattedMessage id="calendar" />,
+      type: 'item',
+      url: '/apps/calendar',
+      icon: icons.CalendarOutlined
+    },
+  ]
+};
+
+export default other;
